58ab8d0613eec47b21ebb80aed379094f741cd75
hasnainali659 <hasnainali.ha659@gmail.com>
2023-12-20 22:24:55 +0500

    Database for Chat, RAG and User

--- LangChainApp/__init__.py
+++ LangChainApp/__init__.py
@@ -10,7 +10,6 @@ from LangChainApp.module4_RAG.views import module4_blueprint
 from LangChainApp.module5_Upload.views import module5_blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from LangChainApp.models import User, Chat, RAG
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -23,10 +22,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 Migrate(app,db)
-
-# Import other module blueprints
-
-app = Flask(__name__)
 
 # Define the upload folder
 UPLOAD_FOLDER = r'D:\ICS_Arabia\ICS_Langchain\LangChainApp\module5_Upload\uploads'
@@ -47,3 +42,52 @@ app.register_blueprint(module5_blueprint)
 @app.route("/")
 def index():
     return "Welcome to the app!"
+
+################################
+
+class User(db.Model):
+    
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    chats = db.relationship('Chat', backref='users', lazy=True)
+    RAGs = db.relationship('RAG', backref='users', lazy=True)
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+class Chat(db.Model):
+    
+    __tablename__ = 'chat_history'
+    
+    users = db.relationship(User)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    response = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+    def __init__(self, question, response, user_id):
+        self.question = question
+        self.response = response
+        self.user_id = user_id
+
+class RAG(db.Model):
+    
+    __tablename__ = 'RAG_Document'
+    
+    users = db.relationship(User)
+    
+    id = db.Column(db.Integer, primary_key=True)
+    doc_path = db.Column(db.String(200), nullable=False)
+    embedding_path = db.Column(db.String(2000), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    
+    def __init__(self, doc_path, embedding_path, user_id):
+        self.doc_path = doc_path
+        self.embedding_path = embedding_path
+        self.user_id = user_id
+    
---
+++ LangChainApp/models_.py
@@ -0,0 +1,44 @@
+from LangChainApp import db
+import uuid
+
+class User(db.Model):
+    
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    chats = db.relationship('Chat', backref='user', lazy=True)
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+class Chat(db.Model):
+    
+    __tablename__ = 'chat_history'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.Text, nullable=False)
+    response = db.Column(db.Text, nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __init__(self, question, response, user_id):
+        self.question = question
+        self.response = response
+        self.user_id = user_id
+
+class RAG(db.Model):
+    
+    __tablename__ = 'RAG_Document'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    doc_path = db.Column(db.String(200), nullable=False)
+    embedding_path = db.Column(db.String(2000), nullable=False)
+    user_id = db.Column(db.Integer, db.ForiegnKey('user.id'), nullable=False)
+    
+    def __init__(self, doc_path, embedding_path, user_id):
+        self.doc_path = doc_path
+        self.embedding_path = embedding_path
+        self.user_id = user_id
+    
